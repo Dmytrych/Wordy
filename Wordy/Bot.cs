cfbbd956338b14fc3ca4bd21d97a77749a02eec3
Dmytro <karambolrul@gmail.com>
2020-07-11 14:18:07 +0300

    Add project files.

---
+++ Wordy/Bot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Wordy
+{
+    class Bot
+    {
+        public TelegramBotClient botClient;
+        public Bot()
+        {
+            botClient = new TelegramBotClient("1159601816:AAEvbVVXheEKOI4rO8tHL0FRZCYjZgWYREs");
+            botClient.OnMessage += HandleMessage;
+            botClient.StartReceiving();
+        }
+        public void HandleMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            ChatId currentChatId = messageEventArgs.Message.Chat.Id;
+            botClient.SendTextMessageAsync(currentChatId, "Ok");
+            Console.WriteLine(messageEventArgs.Message.Text);
+        }
+    }
+}
---
+++ Wordy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Wordy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            while(true)
+            {
+                bot.botClient.StartReceiving();
+            }
+        }
+    }
+}
